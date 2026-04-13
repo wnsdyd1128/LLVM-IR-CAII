3f556c2f367a750ff82c4e2ebfefd864cad80c5d
wnsdyd1128 <mcleroysane19@inha.edu>
2026-04-13 09:29:47 +0000

    feat: GR740 캐시 모델링 구조 정의

---
+++ include/CacheConfig.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <unordered_set>
+namespace GR740Cache
+{
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+constexpr uint32_t LINE_SIZE = 32;
+constexpr uint32_t NUM_WAYS = 4;
+constexpr uint32_t NUM_SETS = 16384;
+constexpr uint32_t TOTAL_LINES = NUM_WAYS * NUM_SETS;
+
+inline uint32_t block_id(uint64_t addr)
+{
+  return addr >> 5;  // Divide by line size (32 bytes = 2^5)
+}
+
+inline uint32_t set_index(uint64_t addr)
+{
+  return block_id(addr) % NUM_SETS;  // Modulo number of sets
+}
+
+struct CacheBlock
+{
+  uint64_t block_id;
+  uint32_t set_idx;
+
+  bool operator==(const CacheBlock & other) const
+  {
+    return block_id == other.block_id;
+  }
+};
+using CacheBlockSet = std::unordered_set<uint64_t>;
+}  // namespace GR740Cache
